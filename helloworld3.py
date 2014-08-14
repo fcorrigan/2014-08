9216266cac0eed5002de90d131ad6bfbaf31bdf1
hsld45x <hsld45x@student.cs.uwaterloo.ca>
2014-08-14 10:41:30 -0400

    ver 3 commit

---
+++ helloworld3.py
@@ -0,0 +1,5 @@
+Python 3.2 (r32:88452, Feb 20 2011, 11:12:31) 
+[GCC 4.2.1 (Apple Inc. build 5664)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> #print hello world
+print(hello world)
